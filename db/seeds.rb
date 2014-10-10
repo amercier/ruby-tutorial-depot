63ffc680ae26c662f8ad8c758c4e7381dc4b43e6
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-10-09 20:33:34 -0700

    Added 2 more products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Product.create(
+  title: 'CoffeeScript',
+  description:
+    %{<p>
+    CoffeeScript is JavaScript done right. It provides all of JavaScript’s
+    functionality wrapped in a cleaner, more succinct syntax. In the first
+    book on this exciting new language, CoffeeScript guru Trevor Burnham shows
+    you how to hold onto all the power and flexibility of JavaScript while
+    writing clearer, cleaner, and safer code.
+    </p>},
+  image_url: 'cs.jpg',
+  price: 36.00,
+)
+
+Product.create(
   title: 'Programming Ruby 1.9',
   description:
     %{<p>
@@ -16,4 +30,19 @@ Product.create(
     </p>},
   image_url: 'ruby.jpg',
   price: 49.95
+)
+
+Product.create(
+  title: 'Rails Test Prescriptions',
+  description:
+    %{<p>
+    Rails Test Prescriptions is a comprehensive guide to testing Rails
+    applications, covering Test-Driven Development from both a theoretical
+    perspective (why to test) and from a practical perspective (how to test
+    effectively). It covers the core Rails testing tools and procedures for
+    Rails 2 and Rails 3, and introduces popular add-ons, including RSpec,
+    Shoulda, Cucumber, Factory Girl, and Rcov.
+    </p>},
+  image_url: 'rtest.jpg',
+  price: 59.95
 )
